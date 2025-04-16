fc4f617b313969837aa7d6199968617b3b312075
Muhammad <muhammad834565@gmail.com>
2025-04-16 20:14:24 +0500

    Update page2.component.ts

--- src/app/ecomerce/page2/page2.component.ts
+++ src/app/ecomerce/page2/page2.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { MarketplaceFilterComponent } from '../requirment for page 2/marketplace-filter/marketplace-filter.component';
+import { HeaderComponent } from '../header/header.component';
+import { Footer1Component } from '../footer1/footer1.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-page2',
   standalone: true,
-  imports: [MarketplaceFilterComponent],
+  imports: [MarketplaceFilterComponent,HeaderComponent,Footer1Component,FooterComponent],
   templateUrl: './page2.component.html',
   styleUrl: './page2.component.css'
 })
